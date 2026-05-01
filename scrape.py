657732b45f7ce43b6d92bd51c19e7e28e4c168a6
rhemmati101 <rhemmati@g.ucla.edu>
2026-04-30 22:25:13 -0700

    tree model w/ most common country in training set as training label

--- scrape.py
+++ scrape.py
@@ -103,7 +103,8 @@ def extract_dish_and_countries(page_text):
         "Saint Helena, Ascension and Tristan da Cunha", "Cook Islands", "Niue", "Tokelau", "Åland Islands", "Ivory Coast", "Côte d'Ivoire",
         "French Southern and Antarctic Lands", "Réunion", "Mayotte", "Guadaloupe", "Martinique", "Saint Pierre and Miquelon",
         "Bermuda", "British Virgin Islands", "Falkland Islands", "Montserrat", "Turks and Caicos Islands", "Virgin Islands",
-        "Guadalope", "Cayman Islands", "Republic of the Congo", "São Tomé and Príncipe", "DR Congo", "American Samoa",  "French Polynesia",
+        "Guadalupe", "Cayman Islands", "Republic of the Congo", "São Tomé and Príncipe", "DR Congo", "American Samoa",  "French Polynesia",
+        "Cocos (Keeling) Islands", "Christmas Island", "Guam"
     }
     
     lines = [line.strip() for line in page_text.split('\n') if line.strip()]
